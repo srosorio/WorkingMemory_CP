8293af450ee58b780371e65ab113c64fad6b4ba8
Sergio Osorio <saosorio@ucdavis.edu>
2026-06-24 20:11:50 -0700

    Updated json files for transcriptions using pupil labs video recordings

--- SternbergTask-PSB/WM_Sternberg_Speech2text.py
+++ SternbergTask-PSB/WM_Sternberg_Speech2text.py
@@ -14,8 +14,8 @@ print("FFmpeg now resolves to:", shutil.which("ffmpeg"))
 # --------------------------
 # --- Configuration ---
 # --------------------------
-participant_id = "cpeeg02"
-session        = "ses03"
+participant_id = "cpeeg01"
+session        = "ses02"
 block          = 'b02'
 runProfile     = "fullSetup"
 
